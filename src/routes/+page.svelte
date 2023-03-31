5e1f0a74ddc6d465bd09004d0853147c741cad2f
colinmills <colinmills@oldrochester.org>
2023-03-31 12:34:14 -0400

    created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,46 @@
 <script>
-  import ExampleButton from '$lib/components/examples/ExampleButton.svelte';
-	import profilePic from '$lib/assets/profile-pic.jpg';
 </script>
 
-<h1>This is your Landing Page To Your Portfolio Template</h1>
-<h2>This is your main landing page</h2>
+<h1>Honoring the Death of Ethan Furtado</h1>
 
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p>Take a look at me!</p>
-<img src='{profilePic}'
-		 alt='profile pic'>
 
-<p>This is an example of an imported button</p>
-<ExampleButton>Click me</ExampleButton>
+<figure>
+<img  id="img"
+			src='https://sippican.theweektoday.com/sites/beaverdam/files/2017/05/field/image/DSCN6953.jpg'>
 
+		<figcaption id='img-caption' > "He was a lonely young boy taken
+																	 from us at an age too young" - Barrack Obama
+																	 </figcaption>
+</figure>
+
+
+<p>Visit <a href="https://sippican.theweektoday.com/node/28659">The life and
+																																tragedy of Ethan Furtado</a> to read the documentation</p>
 <!--All Style is Local To The Page/Component-->
 <style>
 	h1 {
-		color: red;
+      text-align: center;
+			font-size: 50px;
 	}
 
 	h2 {
-		color: green;
+
 	}
 
 	p {
 		color: blue;
 	}
+
+
+
+	figure{
+			margin: auto;
+			text-align: center;
+	}
+  img {
+      margin: auto;
+  }
+
 </style>
