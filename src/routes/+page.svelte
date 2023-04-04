f69cf2ad06bab9d2628b4035611148b89d1b13fb
colinmills <colinmills@oldrochester.org>
2023-04-04 10:01:54 -0400

    created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,13 @@
 																	 </figcaption>
 </figure>
 
+<ul class='tribute-info'>
+	<li> Ethan was a young boy from Rochester MA</li>
+	<li> The young boy was doing an experiment for the science fair when he
+			 was suddenly swallowed by a self made tornado in a bottle</li>
+	<li> This was the last photo ever taken of him</li>
+	<li> He was seen wearing his favorite red under armour shirt</li>
+</ul>
 
 <p>Visit <a href="https://sippican.theweektoday.com/node/28659">The life and
 																																tragedy of Ethan Furtado</a> to read the documentation</p>
