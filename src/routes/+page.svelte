24e140bca5abb4770afa58f0322b8be2f9e2df48
colinmills <colinmills@oldrochester.org>
2023-04-06 09:31:23 -0400

    created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,82 @@
 <script>
 </script>
 
-<h1>Honoring the Death of Ethan Furtado</h1>
+<div class="container">
+	<h1 id="title">Honoring the Death of Ethan Furtado</h1>
 
+	<div id="img-div">
+		<img id="image" src="https://sippican.theweektoday.com/sites/beaverdam/files/2017/05/field/image/DSCN6953.jpg" alt="Ethan Furtado">
 
+		<figcaption id="img-caption">"He was a lonely young boy taken from us at an age too young" - Barrack Obama</figcaption>
+	</div>
 
+	<div id="tribute-info">
+		<ul>
+			<li>Ethan was a young boy from Rochester MA</li>
+			<li>The young boy was doing an experiment for the science fair when he was suddenly swallowed by a self made tornado in a bottle</li>
+			<li>This was the last photo ever taken of him</li>
+			<li>He was seen wearing his favorite red under armour shirt</li>
+		</ul>
+	</div>
 
-<figure>
-<img  id="img"
-			src='https://sippican.theweektoday.com/sites/beaverdam/files/2017/05/field/image/DSCN6953.jpg'>
+	<blockquote>
+		<p>"He was a lonely young boy taken from us at an age too young"</p>
+		<footer>Barrack Obama</footer>
+	</blockquote>
 
-		<figcaption id='img-caption' > "He was a lonely young boy taken
-																	 from us at an age too young" - Barrack Obama
-																	 </figcaption>
-</figure>
+	<p><a id="tribute-link" href="https://sippican.theweektoday.com/node/28659" target="_blank">The life and tragedy of Ethan Furtado</a></p>
+</div>
 
-<ul class='tribute-info'>
-	<li> Ethan was a young boy from Rochester MA</li>
-	<li> The young boy was doing an experiment for the science fair when he
-			 was suddenly swallowed by a self made tornado in a bottle</li>
-	<li> This was the last photo ever taken of him</li>
-	<li> He was seen wearing his favorite red under armour shirt</li>
-</ul>
-
-<p>Visit <a href="https://sippican.theweektoday.com/node/28659">The life and
-																																tragedy of Ethan Furtado</a> to read the documentation</p>
-<!--All Style is Local To The Page/Component-->
 <style>
-	h1 {
-      text-align: center;
-			font-size: 50px;
-	}
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+    }
 
-	h2 {
+    #title {
+        text-align: center;
+        font-size: 50px;
+    }
 
-	}
+    #img-div {
+        text-align: center;
+        margin: 20px 0;
+    }
 
-	p {
-		color: blue;
-	}
+    #image {
+        max-width: 100%;
+        height: auto;
+        display: block;
+        margin: 0 auto;
+    }
 
+    #img-caption {
+        font-style: italic;
+        font-size: 16px;
+        margin-top: 10px;
+    }
 
+    #tribute-info {
+        margin-bottom: 20px;
+    }
 
-	figure{
-			margin: auto;
-			text-align: center;
-	}
-  img {
-      margin: auto;
-  }
+    blockquote {
+        font-style: italic;
+        margin: 20px;
+        padding: 20px;
+        border-left: 5px solid #ccc;
+    }
 
+    footer {
+        margin-top: 10px;
+        text-align: right;
+    }
+
+    #tribute-link {
+        color: blue;
+        font-weight: bold;
+        text-decoration: none;
+    }
 </style>
