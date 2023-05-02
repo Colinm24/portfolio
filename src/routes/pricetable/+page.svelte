c85346914f41bc2507370965edb594ba12206ced
colinmills <colinmills@oldrochester.org>
2023-05-02 12:34:06 -0400

    created

--- src/routes/pricetable/+page.svelte
+++ src/routes/pricetable/+page.svelte
@@ -1,145 +1,318 @@
-<script>
-</script>
-<div class="container">
-	<div class="boxes">
-		<div class="box box-start">
-			<h2 class="title">Basic</h2>
-			<div class="price">
-				<span class="price-value">$9</span><span class="price-billed">/month</span>
+<!-- Global Container -->
+<div class="flex items-center justify-center min-h-screen
+bg-slate-800">
+	<!-- Inner Container -->
+	<div
+		class="flex flex-col my-6 space-y-6 md:space-y-0
+md:space-x-6 md:flex-row md:my-0"
+	>
+		<!-- Col 1 -->
+		<div class="bg-slate-700 rounded-xl text-white">
+			<!-- Upper Container -->
+			<div class="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+				<div class="text-center uppercase">Basic</div>
+				<h2 class="mt-10 font-serif text-5xl
+
+text-center">100GB</h2>
+
+				<h3 class="mt-2 text-center">$1.99/Month</h3>
+				<div class="flex justify-center">
+					<a
+						href="#"
+						class="inline-block px-10 py-3 my-6 text-center
+
+border border-violet-600 rounded-lg duration-200
+hover:bg-violet-800 hover:border-violet-800"
+
+					>Purchase</a
+					>
+				</div>
 			</div>
-			<ul class="features">
-				<li>Shared hosting</li>
-				<li>512mb RAM</li>
-				<li>20gb storage</li>
-				<li>200gb bandwidth</li>
-				<li><s>Choice of OS</s></li>
-				<li><s>Host multiple sites</s></li>
-			</ul>
-			<a href="#" class="button">Get Started</a>
+
+			<!-- Border -->
+			<div class="border-t border-slate-700"></div>
+
+			<!-- Lower Container -->
+			<div class="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+				<!-- List Container -->
+				<div class="flex flex-col space-y-2">
+					<!-- List Item 1 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">100 GB of storage</span>
+					</div>
+
+					<!-- List Item 2 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">Option to add
+
+members</span>
+					</div>
+
+					<!-- List Item 3 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+
+						<span class="text-sm ml-1">Extra member
+
+benefits</span>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="box box-center">
-			<h2 class="title">Professional</h2>
-			<div class="price">
-				<span class="price-value">$18</span><span class="price-billed">/month</span>
+
+		<!-- Col 2 -->
+		<div class="bg-white rounded-xl text-white">
+			<!-- Upper Container -->
+			<div class="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+				<div class="text-center uppercase">Standard</div>
+				<h2 class="mt-10 font-serif text-5xl
+
+text-center">200GB</h2>
+
+				<h3 class="mt-2 text-center">$3.99/Month</h3>
+				<div class="flex justify-center">
+					<a
+						href="#"
+						class="inline-block px-10 py-3 my-6 text-center
+border border-violet-600 rounded-lg duration-200 bg-violet-600
+hover:bg-violet-800 hover:border-violet-800"
+
+					>Purchase</a
+					>
+				</div>
 			</div>
-			<ul class="features">
-				<li>VPS hosting</li>
-				<li>4gb RAM</li>
-				<li>40gb SSD storage</li>
-				<li>1tb bandwidth</li>
-				<li>Choice of OS</li>
-				<li>Host multiple sites</li>
-			</ul>
-			<a href="#" class="button">Upgrade Now</a>
+
+			<!-- Border -->
+			<div class="border-t border-slate-700"></div>
+
+			<!-- Lower Container -->
+
+			<div class="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+				<!-- List Container -->
+				<div class="flex flex-col space-y-2">
+					<!-- List Item 1 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">200 GB of storage</span>
+					</div>
+
+					<!-- List Item 2 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">Option to add
+
+members</span>
+					</div>
+
+					<!-- List Item 3 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">Extra member
+
+benefits</span>
+
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="box box-end">
-			<h2 class="title">Enterprise</h2>
-			<div class="price">
-				<span class="price-value">$36</span><span class="price-billed">/month</span>
+
+		<!-- Col 3 -->
+		<div class="bg-slate-700 rounded-xl text-white">
+			<!-- Upper Container -->
+			<div class="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+				<div class="text-center uppercase">Premium</div>
+				<h2 class="mt-10 font-serif text-5xl text-center">2
+
+																													TB</h2>
+
+				<h3 class="mt-2 text-center">$8.99/Month</h3>
+				<div class="flex justify-center">
+					<a
+						href="#"
+						class="inline-block px-10 py-3 my-6 text-center
+
+border border-violet-600 rounded-lg duration-200
+hover:bg-violet-800 hover:border-violet-800"
+
+					>Purchase</a
+					>
+				</div>
 			</div>
-			<ul class="features">
-				<li>Dedicated hosting</li>
-				<li>12gb RAM</li>
-				<li>120gb SSD storage</li>
-				<li>1tb bandwidth</li>
-				<li>Choice of OS</li>
-				<li>Host multiple sites</li>
-			</ul>
-			<a href="#" class="button">Upgrade Now</a>
+
+			<!-- Border -->
+			<div class="border-t border-slate-700"></div>
+
+			<!-- Lower Container -->
+			<div class="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+				<!-- List Container -->
+
+				<div class="flex flex-col space-y-2">
+					<!-- List Item 1 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">2 TB of storage</span>
+					</div>
+
+					<!-- List Item 2 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">Option to add
+
+members</span>
+					</div>
+
+					<!-- List Item 3 -->
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"
+
+							/>
+
+							<path d="M5 12l5 5l10 -10" />
+						</svg>
+						<span class="text-sm ml-1">Exclusive Benefits for You</span>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	</div>
 </div>
 
 <style>
-    .container {
-        padding: 10% 0;
-        background-color: #276b93;
-    }
-    .boxes {
-        display: flex;
-        align-items: center;
-        max-width: 798px;
-        margin: auto;
-    }
 
-    /* Box */
-    .box {
-        position: relative;
-        flex: 1;
-        font-family: Work Sans, sans-serif;
-        border-radius: 4px;
-        background-color: #ffffff;
-        color: #000000;
-        text-align: center;
-        box-shadow: 0 2px 2px rgba(54, 141, 115, 0.4),
-        0 2px 12px rgba(77, 184, 152, 0.4);
-    }
-    .box-outer {
-        padding: 60px 0;
-    }
-    .box-inner {
-        padding: 72px 0;
-        margin: 0 -12px;
-        z-index: 1;
-    }
-
-    /* Title */
-    .title {
-        color: #000000;
-        font-size: 36px;
-        font-weight: 300;
-        letter-spacing: -0.05em;
-        margin: 0 0 18px;
-    }
-
-    /* Price */
-    .price {
-        padding: 12px;
-        background-color: #fff6e5;
-        color: #29d040;
-        font-weight: 600;
-        font-size: 18px;
-    }
-    .price-value {
-        font-size: 24px;
-    }
-    .price-billed {
-        font-variant: small-caps;
-    }
-
-    /* Features */
-    .features {
-        margin: 0;
-        padding: 24px;
-    }
-    .features s {
-        color: #434948;
-    }
-    .features li {
-        line-height: 2;
-        list-style: none;
-    }
-    .features li:first-child {
-        font-weight: 600;
-    }
-
-    /* Button */
-    .button {
-        display: inline-block;
-        background-color: #3ab641;
-        color: #ffffff;
-        padding: 12px 24px;
-        border-radius: 4px;
-        text-decoration: none;
-        font-weight: 600;
-        transition: 0.3s;
-    }
-    .button:hover,
-    .button:focus {
-        color: #06ce21;
-        box-shadow: inset 0 -44px #ffffff;
-    }
-    .button:active {
-        transform: scale(0.95455);
-    }
 </style>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -10,7 +10,7 @@
 				<p class="max-w-xs text-xs leading-5 tracking-wide text-center text-white md:text-left">Stay up-to-date with the latest electronics news, deals, and trends. Sign up for the Electronics newsletter.</p>
 				<div class="flex flex-col mt-5 space-y-4 md:space-x-3 md:flex-row md:space-y-0">
 					<input type="text" placeholder="Enter your email address" class="p-2 px-4 text-center text-white bg-zinc-800 border border-zinc-600 placeholder:text-xs placeholder:text-center md:text-left placeholder:md:text-left focus:outline-none"/>
-					<button class="px-5 py-3 text-xs rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500">Subscribe</button>
+					<button class="px-5 py-3 text-xs rounded-md text-zinc-800 bg-white border border-zinc-600 hover:bg-zinc-600 hover:text-zinc-800 duration-500">Subscribe</button>
 				</div>
 			</div>
 		</div>
